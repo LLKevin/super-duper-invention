206072d997aeeb3212d660ae33d4fb10a5e253e3
Kevin Lucas <22506970+LLKevin@users.noreply.github.com>
2022-09-14 20:31:00 -0400

    added list of heroes and dynamically load functionality (#2)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from 'src/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,7 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heros = HEROES; //binds an array for binding 
   hero: Hero = {
     id:1,
     name:'Windstorm'
@@ -19,4 +21,8 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  selectedHero?: Hero;
+  onSelect(hero: Hero):void{
+    this.selectedHero = hero;
+  }
 }
