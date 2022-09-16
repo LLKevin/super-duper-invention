fb2d585db158d45d665e57a1b8d7c2399e85f0e7
Kevin <kevinleo.lucas@gmail.com>
2022-09-16 00:23:41 -0400

    added services to the application and learned how dependency injection functions

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from 'src/mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +9,27 @@ import { HEROES } from 'src/mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heros = HEROES; //binds an array for binding 
-  constructor() { }
+  heroes:Hero[] = [];
+
+  constructor(private heroService:HeroService, 
+    private messageService:MessageService) { }
 
   // lifecycle hook: Angular calls this shortly after creating a component
   // good place to put initialization logic.
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes():void{
+    //synchronous signature -- fetch heroes synchronously
+    //this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes)
   }
 
   selectedHero?: Hero;
   onSelect(hero: Hero):void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
